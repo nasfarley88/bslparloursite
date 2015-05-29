170419b8fcd23d0f7ad616d9dd2d7bf26c656928
Nathanael Farley <nasfarley88@gmail.com>
2015-05-29 11:46:42 -0400

    Took function out as it caused problems.

--- manage.py
+++ manage.py
@@ -2,44 +2,44 @@
 import os
 import sys
 
-import re
+# import re
 
-from bslparlour.models import SourceVideo
-from bsldictionary.models import BSLEntry
-from django.db import IntegrityError
+# from bslparlour.models import SourceVideo
+# from bsldictionary.models import BSLEntry
+# from django.db import IntegrityError
 
-def fill_bsl_entry(source_video=None, gloss=None, gloss_index=1, source_video_sha224=None):
-    if source_video_sha224==None:
-        source_video_sha224 = source_video.sha224
-    print "source video sha224 fetched: " + source_video_sha224
+# def fill_bsl_entry(source_video=None, gloss=None, gloss_index=1, source_video_sha224=None):
+#     if source_video_sha224==None:
+#         source_video_sha224 = source_video.sha224
+#     print "source video sha224 fetched: " + source_video_sha224
 
-    # If the video already has a gloss, don't bother with the function
-    if len(BSLEntry.objects.filter(source_video_sha224=source_video_sha224))>0:
-        print "Returning as " + source_video.filename + " already has an entry."
-        return
+#     # If the video already has a gloss, don't bother with the function
+#     if len(BSLEntry.objects.filter(source_video_sha224=source_video_sha224))>0:
+#         print "Returning as " + source_video.filename + " already has an entry."
+#         return
 
-    if source_video==None and (gloss==None or source_video_sha224==None):
-        assert False, "Check your arguments!"
+#     if source_video==None and (gloss==None or source_video_sha224==None):
+#         assert False, "Check your arguments!"
         
-    if gloss==None:
-        gloss = re.sub(r'(.MOV)|(.MP4)|(\(.*\))', '', source_video.filename.upper()).strip()
-        gloss = gloss.replace('_', ' ')
-        gloss = gloss.replace(' ', '-')
-        gloss = gloss.strip()
-    print "gloss generated: " + gloss
-    print "gloss_index 'generated': " + str(gloss_index)
+#     if gloss==None:
+#         gloss = re.sub(r'(.MOV)|(.MP4)|(\(.*\))', '', source_video.filename.upper()).strip()
+#         gloss = gloss.replace('_', ' ')
+#         gloss = gloss.replace(' ', '-')
+#         gloss = gloss.strip()
+#     print "gloss generated: " + gloss
+#     print "gloss_index 'generated': " + str(gloss_index)
     
 
-    try:
-        BSLEntry.objects.create(
-            gloss=gloss,
-            gloss_index=gloss_index,
-            source_video_sha224=source_video_sha224)
-    except IntegrityError:
-        gloss_index += 1
-        fill_bsl_entry(gloss=gloss,
-                       gloss_index=gloss_index,
-                       source_video_sha224=source_video_sha224)
+#     try:
+#         BSLEntry.objects.create(
+#             gloss=gloss,
+#             gloss_index=gloss_index,
+#             source_video_sha224=source_video_sha224)
+#     except IntegrityError:
+#         gloss_index += 1
+#         fill_bsl_entry(gloss=gloss,
+#                        gloss_index=gloss_index,
+#                        source_video_sha224=source_video_sha224)
         
 
 if __name__ == "__main__":
